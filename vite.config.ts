448fe3dfd07afbcfaa064281ba2805627d62473a
VisualSource <boomishere_network@outlook.com>
2022-12-01 22:48:33 -0500

    fix imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { visualizer } from "rollup-plugin-visualizer";
 import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 
@@ -7,14 +8,20 @@ import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),mdx({
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeSlug]
-  }),
-  VitePWA({
-    registerType: 'autoUpdate',
-    manifest: false
-  })
-]
-})
+export default defineConfig(({ command, mode })=>{
+  const plugins = [
+      react(),
+      mdx({
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeSlug]
+      }),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: false
+      })
+  ]
+
+  if(process.env?.ANALYZE === "TRUE") plugins.push(visualizer({ open: true }));
+
+  return { plugins };
+});
