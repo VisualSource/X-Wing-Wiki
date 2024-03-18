c9bf2b56bcd1334a55bbfeac9031d52862637ce4
VisualSource <collin_blosser@yahoo.com>
2024-03-18 19:02:01 -0400

    remove inpect plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
 import { visualizer } from "rollup-plugin-visualizer";
 import { VitePWA } from 'vite-plugin-pwa';
 import Unfonts from 'unplugin-fonts/vite';
-import Inspect from 'vite-plugin-inspect';
 import react from '@vitejs/plugin-react';
 import rehypeSlug from 'rehype-slug';
 import { defineConfig } from 'vite';
 import remarkGfm from 'remark-gfm';
 import mdx from '@mdx-js/rollup';
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const plugins = [
-    Inspect({
-      build: true,
-    }),
     react(),
     mdx({
       remarkPlugins: [remarkGfm],
