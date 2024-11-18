7cf238848096ad018c45bad50ed829d6e70ff19a
VisualSource <collin_blosser@yahoo.com>
2024-11-18 16:16:17 -0500

    add dev client

--- app/(content)/_layout.tsx
+++ app/(content)/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 
 export default function ContentLayout() {
     useKeepAwake();
-    return <Stack screenOptions={{
-        headerTitle: "Search",
-    }} />
+    return (
+        <Stack>
+            <Stack.Screen name="/loadouts/[id]" />
+            <Stack.Screen name="/rules/[id]" />
+        </Stack>
+    );
 }
